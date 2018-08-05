61d05b128f5f55bc61a92f5f1d88d6cc372d05b4
batu <304701204@qq.com>
2018-08-05 22:32:36 +0800

    add UISwitch

---
+++ _UISwitch 开关控件/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  _UISwitch 开关控件
+//
+//  Created by 张智 on 2018/8/5.
+//  Copyright © 2018年 gvitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    UISwitch* _swc;
+}
+
+@end
+
---
+++ _UISwitch 开关控件/ViewController.m
@@ -0,0 +1,52 @@
+//
+//  ViewController.m
+//  _UISwitch 开关控件
+//
+//  Created by 张智 on 2018/8/5.
+//  Copyright © 2018年 gvitech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+-(void) switchChanged:(UISwitch*)swc{
+    if(swc.on)
+    {
+        self.view.backgroundColor = [UIColor orangeColor];
+    }
+    else
+    {
+        self.view.backgroundColor = [UIColor yellowColor];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _swc = [[UISwitch alloc] init];
+    
+    //UISWitch的大小是固定的，所以size设置对其无效
+    _swc.frame = CGRectMake(100, 100, 0, 0);
+    
+    //设置状态,是否动画
+    [_swc setOn:YES animated:YES];
+    _swc.on = YES;
+    
+    //添加事件
+    //action:@selector(switchChanged:) 一个带参的事件，参数就是switch对象
+    //forControlEvents:UIControlEventValueChanged 控件发生变化时
+    [_swc addTarget:self action:@selector(switchChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:_swc];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
